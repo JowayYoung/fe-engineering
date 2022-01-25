8740e9df26812a7c1176a909b8c54362a8f1f382
JowayYoung <young.joway@aliyun.com>
2022-01-25 23:38:49 +0800

    更新配置

--- commit-lint/custom-config/cz-config.js
+++ commit-lint/custom-config/cz-config.js
@@ -1,27 +1,27 @@
 module.exports = {
 	allowBreakingChanges: ["feat", "fix"],
 	allowCustomScopes: true,
-	scopes: [123],
+	scopes: [],
 	types: [{
-		name: "feat	功能：新增功能，迭代项目需求",
+		name: "feat 功能：新增功能，迭代项目需求",
 		value: "feat"
 	}, {
 		name: "fix 修复：修复缺陷，修复上一版本存在问题",
 		value: "fix"
 	}, {
-		name: "docs	文档：更新文档，仅改动文档不改动代码",
+		name: "docs 文档：更新文档，仅改动文档不改动代码",
 		value: "docs"
 	}, {
 		name: "style 样式：变动格式，不影响代码逻辑",
 		value: "style"
 	}, {
-		name: "refactor	重构：重构代码，非新增功能也非修改缺陷",
+		name: "refactor 重构：重构代码，非新增功能也非修改缺陷",
 		value: "refactor"
 	}, {
-		name: "perf	性能：优化性能，提高代码执行性能",
+		name: "perf 性能：优化性能，提高代码执行性能",
 		value: "perf"
 	}, {
-		name: "test	测试：新增测试，追加测试用例验证代码",
+		name: "test 测试：新增测试，追加测试用例验证代码",
 		value: "test"
 	}, {
 		name: "build 构建：更新构建，改动构建工具或外部依赖",
@@ -39,7 +39,7 @@ module.exports = {
 		name: "merge 合并：合并分支，合并分支代码到其他分支",
 		value: "merge"
 	}, {
-		name: "sync	同步：同步分支，同步分支代码到其他分支",
+		name: "sync 同步：同步分支，同步分支代码到其他分支",
 		value: "sync"
 	}, {
 		name: "impr 改进：改进功能，升级当前功能模块",
