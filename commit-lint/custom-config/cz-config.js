0f4eba4fd762ece0e8a7d68578994e8a8b1174c0
JowayYoung <young.joway@aliyun.com>
2022-01-26 00:00:13 +0800

    更新cz配置

--- commit-lint/custom-config/cz-config.js
+++ commit-lint/custom-config/cz-config.js
@@ -3,46 +3,46 @@ module.exports = {
 	allowCustomScopes: true,
 	scopes: [],
 	types: [{
-		name: "feat 功能：新增功能，迭代项目需求",
+		name: "功能：新增功能，迭代项目需求 (feat)",
 		value: "feat"
 	}, {
-		name: "fix 修复：修复缺陷，修复上一版本存在问题",
+		name: "修复：修复缺陷，修复上一版本存在问题 (fix)",
 		value: "fix"
 	}, {
-		name: "docs 文档：更新文档，仅改动文档不改动代码",
+		name: "文档：更新文档，仅改动文档不改动代码 (docs)",
 		value: "docs"
 	}, {
-		name: "style 样式：变动格式，不影响代码逻辑",
+		name: "样式：变动格式，不影响代码逻辑 (style)",
 		value: "style"
 	}, {
-		name: "refactor 重构：重构代码，非新增功能也非修改缺陷",
+		name: "重构：重构代码，非新增功能也非修改缺陷 (refactor)",
 		value: "refactor"
 	}, {
-		name: "perf 性能：优化性能，提高代码执行性能",
+		name: "性能：优化性能，提高代码执行性能 (perf)",
 		value: "perf"
 	}, {
-		name: "test 测试：新增测试，追加测试用例验证代码",
+		name: "测试：新增测试，追加测试用例验证代码 (test)",
 		value: "test"
 	}, {
-		name: "build 构建：更新构建，改动构建工具或外部依赖",
+		name: "构建：更新构建，改动构建工具或外部依赖 (build)",
 		value: "build"
 	}, {
-		name: "ci 脚本：更新脚本，改动CI或执行脚本配置",
+		name: "脚本：更新脚本，改动CI或执行脚本配置 (ci)",
 		value: "ci"
 	}, {
-		name: "chore 事务：变动事务，改动其他不影响代码的事务",
+		name: "事务：变动事务，改动其他不影响代码的事务 (chore)",
 		value: "chore"
 	}, {
-		name: "revert 回滚：回滚版本，撤销某次代码提交",
+		name: "回滚：回滚版本，撤销某次代码提交 (revert)",
 		value: "revert"
 	}, {
-		name: "merge 合并：合并分支，合并分支代码到其他分支",
+		name: "合并：合并分支，合并分支代码到其他分支 (merge)",
 		value: "merge"
 	}, {
-		name: "sync 同步：同步分支，同步分支代码到其他分支",
+		name: "同步：同步分支，同步分支代码到其他分支 (sync)",
 		value: "sync"
 	}, {
-		name: "impr 改进：改进功能，升级当前功能模块",
+		name: "改进：改进功能，升级当前功能模块 (impr)",
 		value: "impr"
 	}]
 };
