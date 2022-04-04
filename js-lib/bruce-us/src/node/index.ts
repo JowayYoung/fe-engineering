e41217cf0707f29a69a148b93bd0c1ae72fcbacf
JowayYoung <young.joway@aliyun.com>
2022-04-04 15:25:58 +0800

    增加bruce-us模板

---
+++ js-lib/bruce-us/src/node/index.ts
@@ -0,0 +1,33 @@
+import {
+	AbsPath,
+	CopyDir,
+	CreateDir,
+	ReadDir,
+	ReadJson,
+	RemoveDir
+} from "./fs";
+import {
+	GetIP
+} from "./os";
+import {
+	RunCmd
+} from "./process";
+import {
+	NodeType
+} from "./type";
+
+export {
+	// ✅Fs文件
+	AbsPath,
+	CopyDir,
+	CreateDir,
+	ReadDir,
+	ReadJson,
+	RemoveDir,
+	// ✅Os系统
+	GetIP,
+	// ✅Process进程
+	RunCmd,
+	// ✅Type类型
+	NodeType
+};
